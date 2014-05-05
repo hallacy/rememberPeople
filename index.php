4901fdf5f40fe7fd40b4cf9ec68c42508e6da26c
kverdone <kverdone@gmail.com>
2014-05-05 00:10:31 -0700

    Alignment Spans

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 <html>
 	<head>
 		<script src='http://code.jquery.com/jquery.js'></script>
+
+		<style type="text/css">
+			span { width:50%; display:inline-block; }
+    		span.align-right { text-align:right; }
+		</style>
+
 		<title>Remember People</title>
 	</head>
 
@@ -15,12 +21,15 @@
 
 			$people = listPeople();
 
-			foreach($people as $person {
+			foreach($people as $person) {
 		?>
 
-		<a href class="bin"><?php echo $person[0] . " || Last Contact: " . $person[1]; ?></a>
-			<p>Some inside content</p><br>
-			<p>Lorem Ipsum yatta yatta</p><br>
+
+		<a href="bin"><span><?php echo $person[0]; ?></span><span class="align-right">Last Contact: <?php echo $person[1]; ?></span></a>
+			<div>
+				<p>Some inside content</p><br>
+				<p>Lorem Ipsum yatta yatta</p><br>
+			</div>
 			<br>
 
 		<?php } ?>
